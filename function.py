8124a812fb75b62f09b385452cbf5d6ee74d42df
techrebel <Successzero7@gmail.com>
2025-03-19 08:26:34 +0100

    Modify the python code

--- function.py
+++ function.py
@@ -1,10 +1,11 @@
 
 def calculate_discount(price,discount_percent):
-    price = int(input("What is the Price of Goods\n"))
-    discount_percent = int(input("What is the Price of Goods\n"))/100
     if discount_percent >= 0.2:
         return price + price*discount_percent
     else:
         return price
 
-print(calculate_discount(200,10))
+price = int(input("What is the Price of Goods\n"))
+discount_percent = int(input("What is the Price of Goods\n"))/100
+Total_amount =(calculate_discount(price,discount_percent))
+print(Total_amount)
