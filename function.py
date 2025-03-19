d9b73bb13eb9b9abd37977d1b258f2d0991351f7
techrebel <Successzero7@gmail.com>
2025-03-19 08:48:31 +0100

    Adding comment to the code lines

--- function.py
+++ function.py
@@ -1,11 +1,16 @@
 
 def calculate_discount(price,discount_percent):
+    ## the price and discount_percentage here is different from the price and discount_percentage in the try and except 
+    ## if the discount is greater than 20%
     if discount_percent >= 0.2:
         return price + price*discount_percent
     else:
-        return price
+        return price # give back the normal price 
 
-price = int(input("What is the Price of Goods\n"))
-discount_percent = int(input("What is the Price of Goods\n"))/100
-Total_amount =(calculate_discount(price,discount_percent))
-print(Total_amount)
+try:
+    price = float(input("What is the Price of Goods?:\n"))
+    discount_percent = float(input("What is the discount for the Goods?:\n"))/100
+    Total_amount =(calculate_discount(price,discount_percent))
+    print(Total_amount)
+except ValueError:
+    print("Invalid input. Please enter numerical values for price and discount percentage.")
